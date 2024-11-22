abef533543ae8b7773a9c631356f4e8fa7fba468
Aaron Lee <aaronlee9303@gmail.com>
2024-11-22 17:19:03 +0900

    remove blog

--- blog/src/main/kotlin/me/aaronlee/blog/dto/FeedBodyDto.kt
+++
@@ -1,7 +0,0 @@
-package me.aaronlee.blog.dto
-
-data class FeedBodyDto(
-    var createdBy: String,
-    var title: String,
-    var content: String
-)
--- blog/src/main/kotlin/me/aaronlee/blog/repository/FeedRepository.kt
+++
@@ -1,9 +0,0 @@
-package me.aaronlee.blog.repository
-
-import me.aaronlee.blog.entity.Feed
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface FeedRepository : JpaRepository<Feed, Long> {
-}
