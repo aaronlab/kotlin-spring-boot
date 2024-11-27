565c369061d497230d640b1d2b317cbfbb5d7c65
Aaron Lee <aaronlab.net@gmail.com>
2024-11-27 21:15:27 +0900

    Filter by column

--- course-catalog-service/src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
+++ course-catalog-service/src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
@@ -4,4 +4,7 @@ import com.kotlinspring.entity.Course
 import org.springframework.data.repository.CrudRepository
 
 interface CourseRepository: CrudRepository<Course, Int> {
+
+    fun findByNameContaining(name: String): List<Course>
+
 }
---
+++ course-catalog-service/src/test/intg/com/kotlinspring/repository/CourseRepositoryIntgTest.kt
@@ -0,0 +1,38 @@
+package com.kotlinspring.repository
+
+import com.kotlinspring.util.courseEntityList
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.ActiveProfiles
+import kotlin.test.assertEquals
+
+@DataJpaTest
+@ActiveProfiles("test")
+class CourseRepositoryIntgTest {
+
+    @Autowired
+    lateinit var courseRepository: CourseRepository
+
+    @BeforeEach
+    fun setUp() {
+        courseRepository.deleteAll()
+        val courses = courseEntityList()
+        courseRepository.saveAll(courses)
+    }
+
+    @Test
+    fun findByNameContaining() {
+        val courses = courseRepository.findByNameContaining("SpringBoot")
+
+        assert(courses.isNotEmpty())
+        assertEquals(
+            courseEntityList()
+                .filter { it.name.contains("SpringBoot", ignoreCase = true) }
+                .size,
+            courses.size
+        )
+    }
+
+}
