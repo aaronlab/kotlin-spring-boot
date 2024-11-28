2151795c49d33388fa351da4a4a8e2d70f0a0c4b
Aaron Lee <aaronlab.net@gmail.com>
2024-11-28 21:46:16 +0900

    POST Instructor integration test

--- course-catalog-service/src/test/intg/com/kotlinspring/util/TestUtil.kt
+++ course-catalog-service/src/test/intg/com/kotlinspring/util/TestUtil.kt
@@ -1,7 +1,9 @@
 package com.kotlinspring.util
 
 import com.kotlinspring.dto.CourseDTO
+import com.kotlinspring.dto.InstructorDTO
 import com.kotlinspring.entity.Course
+import com.kotlinspring.entity.Instructor
 
 fun courseEntityList() = listOf(
     Course(
@@ -29,4 +31,27 @@ fun courseDTO(
     id,
     name,
     category
+)
+
+fun instructorEntityList() = listOf(
+    Instructor(
+        null,
+        "John Doe"
+    ),
+    Instructor(
+        null,
+        "Jane Doe"
+    ),
+    Instructor(
+        null,
+        "John Smith"
+    )
+)
+
+fun instructorDTO(
+    id: Int? = null,
+    name: String = "John Doe"
+) = InstructorDTO(
+    id,
+    name
 )
