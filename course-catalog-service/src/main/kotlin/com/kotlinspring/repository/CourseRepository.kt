4a2a6114c945aef7e06afc575a359b7e054e9e4b
Aaron Lee <aaronlab.net@gmail.com>
2024-11-28 21:41:42 +0900

    POST Instructors

--- course-catalog-service/src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
+++ course-catalog-service/src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
@@ -4,7 +4,7 @@ import com.kotlinspring.entity.Course
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
-interface CourseRepository: CrudRepository<Course, Int> {
+interface CourseRepository : CrudRepository<Course, Int> {
 
     fun findByNameContaining(name: String): List<Course>
 
