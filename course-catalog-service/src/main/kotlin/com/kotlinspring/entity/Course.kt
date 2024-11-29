107178e139f81017cd32cb278ad3ec61a28cc8b0
Aaron Lee <aaronlab.net@gmail.com>
2024-11-29 20:01:30 +0900

    Validate instructor data

--- course-catalog-service/src/main/kotlin/com/kotlinspring/entity/Course.kt
+++ course-catalog-service/src/main/kotlin/com/kotlinspring/entity/Course.kt
@@ -21,4 +21,8 @@ data class Course(
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "instructor_id", nullable = false)
     var instructor: Instructor? = null
-)
+) {
+    override fun toString(): String {
+        return "Course(id=$id, name='$name', category='$category', instructor=${instructor?.id})"
+    }
+}
