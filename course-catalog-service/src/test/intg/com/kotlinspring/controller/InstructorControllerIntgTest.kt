a4949e4e5808fbfed50dbacea8c379cf78381b21
Aaron Lee <aaronlab.net@gmail.com>
2024-11-29 22:27:29 +0900

    DataJpaTest

--- course-catalog-service/src/test/intg/com/kotlinspring/controller/InstructorControllerIntgTest.kt
+++ course-catalog-service/src/test/intg/com/kotlinspring/controller/InstructorControllerIntgTest.kt
@@ -1,12 +1,17 @@
 package com.kotlinspring.controller
 
 import com.kotlinspring.dto.InstructorDTO
+import com.kotlinspring.repository.CourseRepository
 import com.kotlinspring.repository.InstructorRepository
+import com.kotlinspring.util.PostgreSQLContainerInitializer
+import com.kotlinspring.util.courseEntityList
+import com.kotlinspring.util.instructorEntity
 import com.kotlinspring.util.instructorEntityList
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
@@ -15,7 +20,8 @@ import org.springframework.test.web.reactive.server.WebTestClient
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @AutoConfigureWebTestClient
-class InstructorControllerIntgTest {
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class InstructorControllerIntgTest : PostgreSQLContainerInitializer() {
 
     @Autowired
     lateinit var webTestClient: WebTestClient
@@ -23,11 +29,19 @@ class InstructorControllerIntgTest {
     @Autowired
     lateinit var instructorRepository: InstructorRepository
 
+    @Autowired
+    lateinit var courseRepository: CourseRepository
+
     @BeforeEach
     fun setUp() {
+        courseRepository.deleteAll()
         instructorRepository.deleteAll()
-        val instructors = instructorEntityList()
-        instructorRepository.saveAll(instructors)
+
+        val instructor = instructorEntity()
+        instructorRepository.save(instructor)
+
+        val courses = courseEntityList(instructor)
+        courseRepository.saveAll(courses)
     }
 
     @Test
